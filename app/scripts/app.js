2f430806c82e2a3638d8983b570edb0d04cd9072
Jamie Taylor <JayTizzle@Jamies-MacBook-Air.local>
2015-12-25 20:45:33 +0000

    Removal of deprecated code
    Removed some deprecated JavaScript

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,42 +18,38 @@ angular
     'ngTouch',
     'ui.router'
   ])
-  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'views/home.html',
-        controller: 'homeCtrl'
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html',
-        controller: 'aboutCtrl'
-      })
-      .state('history', {
-        url: '/band-info/band-history',
-        templateUrl: 'views/band-info/band-history.html',
-        controller: 'bandHistoryCtrl'
-      })
-      .state('members', {
-        url: '/band-info/band-members',
-        templateUrl: 'views/band-info/band-members.html',
-        controller: 'bandMembersCtrl'
-      })
-      .state('equipment' ,{
-        url:'/band-info/band-equipment',
-        templateUrl: 'views/band-info/band-equipment.html',
-        controller: 'bandEquipmentCrtl'
-      })
-      .state('recordings', {
-        url : '/band-info/band-recordings',
-        templateUrl : '/views/band-info/band-recordings.html',
-        controller : 'bandRecordingsCtrl'
-      })
-      .state('shows', {
-        url: '/shows',
-        templateUrl:'/views/shows.html',
-        controller: 'showsCtrl'
-      });
+  .config(['$urlRouterProvider', '$stateProvider',
+    function($urlRouterProvider, $stateProvider) {
+      $urlRouterProvider.otherwise('/');
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'views/home.html',
+          controller: 'homeCtrl'
+        })
+        .state('history', {
+          url: '/band-info/band-history',
+          templateUrl: 'views/band-info/band-history.html',
+          controller: 'bandHistoryCtrl'
+        })
+        .state('members', {
+          url: '/band-info/band-members',
+          templateUrl: 'views/band-info/band-members.html',
+          controller: 'bandMembersCtrl'
+        })
+        .state('equipment' ,{
+          url:'/band-info/band-equipment',
+          templateUrl: 'views/band-info/band-equipment.html',
+          controller: 'bandEquipmentCrtl'
+        })
+        .state('recordings', {
+          url : '/band-info/band-recordings',
+          templateUrl : '/views/band-info/band-recordings.html',
+          controller : 'bandRecordingsCtrl'
+        })
+        .state('shows', {
+          url: '/shows',
+          templateUrl:'/views/shows.html',
+          controller: 'showsCtrl'
+        });
   }]);
--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -1,20 +1,7 @@
 'use strict';
 
 angular.module('rebesWebsiteApp')
-  .controller('homeCtrl', ['$scope', /*'Friends',*/ function($scope/*, Friends*/) {
+  .controller('homeCtrl', ['$scope', function($scope) {
+    $('#loading-spinner').hide();
     $scope.title = 'Home';
-    // Friends.get().then(function(data) {
-    //   $scope.friends = data;
-    // });
-    $scope.items = ['home','about','contact'];
-
-    // selectedValue is used on the page which implements this controller.
-    // It is used to preselect a value from the above item array when applying
-    // that array to a select
-    $scope.selectedValue = 'home';
-
-    // here we're finally applying the data provided by friends to the scope
-    // Friends.get().then(function(data) {
-    //   $scope.friends = data;
-    // });
   }]);
